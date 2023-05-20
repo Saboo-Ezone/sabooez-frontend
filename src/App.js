1ab79c1793ac85c097ad7097bf5cf3c267fb2241
salman170 <salmanansari02120@gmail.com>
2023-05-20 13:22:35 +0530

    photos uploaded

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-const App = () => {
+import Header from './components/otherComponents/Header';
+import Footer from './components/otherComponents/Footer';
+import HomePage from './pages/HomePage';
+import About from './pages/About';
+import Tech from './pages/Tech';
+import News from './pages/News';
+import Contact from './pages/Contact';
+import SwitchE1 from './components/vehicles/SwitchE1';
+import Solo from './components/vehicles/Solo';
+import Metrocity from './components/vehicles/Metrocity';
+import Metrodecker from './components/vehicles/Metrodecker';
+
+function App() {
   return (
-    <div >App</div>
-  )
+    <>
+      <div><Header/> </div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route exact path='/about' element={<About />} />
+          <Route exact path='/tech' element={<Tech />} />
+          <Route exact path='/news' element={<News />} />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/e1' element={<SwitchE1 />} />
+          <Route exact path='/metrocity' element={<Metrocity />} />
+          <Route exact path='/metrodecker' element={<Metrodecker />} />
+          <Route exact path='/solo' element={<Solo />} />
+        </Routes>
+      </Router>
+      <div><Footer/> </div>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/vehicles/SwitchE1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SwitchE1 = () => {
+  return (
+    <div>SwitchE1</div>
+  )
+}
+
+export default SwitchE1
