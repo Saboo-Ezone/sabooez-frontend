b041da4ef7d6b03e259c788022bf7c9c619690dd
salman170 <salmanansari02120@gmail.com>
2023-05-22 17:55:54 +0530

    logo updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,6 @@ import patternBoxSvg from '../assets/other/pattern-box.svg';
 import patternWhite from '../assets/other/pattern-grid-white.png';
 import bannerCotact from '../assets/other/banner-contact.jpg';
 
-
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import SubmitForm from '../components/otherComponents/SubmitForm';
@@ -19,7 +18,7 @@ const Contact = () => {
         style={{ backgroundImage: `url(${patternBoxSvg})` }}
       >
         <div className='container  max-w-full mx-auto sm:px-2'>
-        <div className='flex flex-wrap justify-around '>
+          <div className='flex flex-wrap justify-around '>
             <div className='w-4/5 md:w-2/5  xl:w-2/5 pr-2 md:pr-4 md:pl-4 mb-4 mt-8 offset-1 xl:mx-1/5 '>
               <div
                 className='text-3xl lg:text-5xl md:pt-12 pt-4 md:pl-12 pb-12'
@@ -46,17 +45,21 @@ const Contact = () => {
               data-aos-delay='300'
               className='md:w-1/2 md:px-4 px-5 xl:w-1/3 xl:mx-1/6 xl:pl-0'
             >
-             <SubmitForm/>
+              <SubmitForm />
             </div>
           </div>
         </div>
       </section>
-      <section className='bg-[#edede6] ' style={{ backgroundImage: `url(${patternWhite})` }}>
+      <section
+        className='bg-[#edede6] '
+        style={{ backgroundImage: `url(${patternWhite})` }}
+      >
         <div className='flex  flex-col md:flex-row justify-between xl:h-[70vh]'>
           <div className='py-10 font-roboto'>
             <div
               className='text-4xl lg:text-5xl pb-6 lg:pb-12 pt-12 text-left pr-2 pl-8 md:pl-16 lg:pl-20'
               data-aos='fade-up'
+              data-aos-delay='500'
             >
               Here to help You
             </div>
@@ -65,12 +68,16 @@ const Contact = () => {
                 <p
                   className='text-indigo-500 pb-8 text-xl'
                   data-aos='fade-up'
-                  data-aos-delay='300'
+                  data-aos-delay='500'
                 >
                   If you've got a question, drop us a line or give us a call.
                 </p>
 
-                <div data-aos='fade-up' data-aos-delay='700' className='text-xl'>
+                <div
+                  data-aos='fade-up'
+                  data-aos-delay='500'
+                  className='text-xl'
+                >
                   <p>
                     <div className=' text-lg  lg:text-2xl underline-offset-4 underline'>
                       Saboo RKS Motor - Maruti Suzuki Arena
@@ -93,10 +100,12 @@ const Contact = () => {
             </div>
           </div>
 
-          <div className='w-full bg-no-repeat bg-cover md:w-1/2 h-[40vh] md:h-auto bg-center' style={{ backgroundImage: `url(${bannerCotact})` }}>
-          </div>
-          
-            
+          <div
+            data-aos='fade-up'
+            data-aos-delay='700'
+            className='w-full bg-no-repeat bg-cover md:w-1/2 h-[40vh] md:h-auto bg-center'
+            style={{ backgroundImage: `url(${bannerCotact})` }}
+          ></div>
         </div>
       </section>
     </div>
