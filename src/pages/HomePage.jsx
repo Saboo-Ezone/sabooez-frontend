3b1f6da4bfa738eb7bc754473c9cb7abaf87c31b
salman170 <salmanansari02120@gmail.com>
2023-05-22 15:44:00 +0530

    homepage done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,10 +8,15 @@ import metrodecker from '../assets/vehicle/metrodecker/metrodecker-lightest-bus.
 import metrocity from '../assets/vehicle/metrocity/metrocity-mirror.jpg';
 import bannerTech from '../assets/other/banner-tech.jpg';
 import patternGreen from '../assets/other/pattern-green.png';
+import patternPng from '../assets/other/pattern.png';
+import patternBoxSvg from '../assets/other/pattern-box.svg';
+import switchInfographic from '../assets/other/Switch-Infographic_updated3860.png';
+import switchInfographicupdated from '../assets/other/Switch-Infographic_updated-mob3860.jpg';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { Link } from 'react-router-dom';
+import SubmitForm from '../components/otherComponents/SubmitForm';
 
 const HomePage = () => {
   useEffect(() => {
@@ -171,78 +176,136 @@ const HomePage = () => {
         ></div>
         <div
           style={{ backgroundImage: `url(${patternGreen})` }}
-          class='w-full md:w-1/2 h-[65vh] flex flex-col justify-between p-6 sm:p-8 lg:p-12 text-white'
+          class='w-full md:w-1/2 h-[45vh] md:h-[65vh] flex flex-col justify-between p-6 sm:p-8 lg:p-12 text-white'
         >
           <div className='flex flex-wrap justify-between items-center'>
-            <div className='text-h1'>A Connected World</div>
-            <div>TECHNOLOGY</div>
+            <div className='text-h1' data-aos='fade-up' data-aos-delay='100'>
+              A Connected World
+            </div>
+            <div data-aos='fade-up' data-aos-delay='200'>
+              TECHNOLOGY
+            </div>
           </div>
-          <div className='flex flex-wrap lg:flex-nowrap justify-between items-center'>
-            <div className='text-lg'>
+          <div
+            className='flex flex-col xl:flex-row justify-between '
+            data-aos='fade-up'
+            data-aos-delay='100'
+          >
+            <div className='text-lg xl:pr-6'>
               Powered by SWITCH proprietary tech and the latest innovations in
               electromobility.
             </div>
-            <div className=' pb-10 md:text-end text-center mt-6 whi' data-aos='fade-up'
-              data-aos-delay='200'>
-                <Link
-                  to='/tech'
-                  className='relative px-8 py-2 text-xl lg:text-2xl overflow-hidden font-medium text-white border-[3px] border-gray-100 shadow-inner group'
-                >
-                  <span className='absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-indigo-400 group-hover:w-full ease'></span>
-                  <span className='absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-indigo-400 group-hover:w-full ease'></span>
-                  <span className='absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-indigo-400 group-hover:h-full ease'></span>
-                  <span className='absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-indigo-400 group-hover:h-full ease'></span>
-                  <span className='absolute inset-0 w-full h-full duration-300 delay-300 bg-white opacity-0 group-hover:opacity-100'></span>
-                  <span className='relative transition-colors duration-300 delay-200 group-hover:text-indigo-500 ease'>
-                    DISCOVER
-                  </span>
-                </Link>
-              </div>
-          </div>
-          {/* <div> A Connected World</div> */}
-          {/* <div class='flex flex-col justify-between h-full px-6 md:px-12 py-8 object-cover bg-repeat'>
             <div
-              class='flex flex-col md:flex-row items-center md:justify-between mb-8'
+              className=' pb-10 mt-6 whitespace-nowrap'
               data-aos='fade-up'
+              data-aos-delay='200'
             >
-              <div class='md:w-2/3'>
-                <h2
-                  className='text-white  leading-tight text-h1 mt-10  '
-                  data-editor-processed='true'
-                >
-                  A Connected World
+              <Link
+                to='/tech'
+                className='relative px-6 py-2 text-xl  overflow-hidden font-medium text-white border-[2px] border-gray-100 shadow-inner group'
+              >
+                <span className='absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-1 border-indigo-400 group-hover:w-full ease'></span>
+                <span className='absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-1 border-indigo-400 group-hover:w-full ease'></span>
+                <span className='absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-indigo-400 group-hover:h-full ease'></span>
+                <span className='absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-indigo-400 group-hover:h-full ease'></span>
+                <span className='absolute inset-0 w-full h-full duration-300 delay-300 bg-white opacity-0 group-hover:opacity-100'></span>
+                <span className='relative transition-colors duration-300 delay-200 group-hover:text-indigo-500 ease'>
+                  DISCOVER
+                </span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='mb-1 flex justify-center'>
+        <img
+          src={switchInfographic}
+          className='hidden sm:block'
+          alt='Switch infographic'
+        />
+
+        <img
+          src={switchInfographicupdated}
+          className='block w-full sm:hidden'
+          alt='switchInfographicupdated'
+        />
+      </section>
+      <section
+        className='py-6 bg-[#505bfe] text-center text-white flex items-center min-h-[70vh]'
+        style={{ backgroundImage: `url(${patternPng})` }}
+      >
+        <div className='container mx-auto'>
+          <div className='section-heading' data-aos='fade-up'>
+            <h2
+              className='text-3xl lg:text-5xl    pb-6'
+              data-editor-processed='true'
+            >
+              SWITCH Up
+            </h2>
+            <p
+              className='text-xl lg:text-2xl font-medium pb-10 font-mono  '
+              data-aos='fade-up'
+              data-aos-delay='100'
+              data-editor-processed='true'
+            >
+              Mobility innovation backed by over 100 years of experience. <br />
+              Find out how we're switching things up.
+            </p>
+          </div>
+
+          <Link
+            to='/about'
+            class='relative px-8 py-2 text-xl lg:text-2xl overflow-hidden font-medium text-white border-[2px] border-gray-100 shadow-inner group'
+            data-editor-processed='true'
+            data-aos='fade-up'
+            data-aos-delay='300'
+          >
+            <span class='absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-indigo-400 group-hover:w-full ease'></span>
+            <span class='absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-indigo-400 group-hover:w-full ease'></span>
+            <span class='absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-100 bg-indigo-400 group-hover:h-full ease'></span>
+            <span class='absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-100 bg-indigo-400 group-hover:h-full ease'></span>
+            <span class='absolute inset-0 w-full h-full duration-300 delay-200 bg-white opacity-0 group-hover:opacity-100'></span>
+            <span class='relative transition-colors duration-300 delay-200 group-hover:text-indigo-500 ease'>
+              OUR STORY
+            </span>
+          </Link>
+        </div>
+      </section>
+      <section
+        className='intro-newsletter generic-padding flex bg-no-repeat mt-4'
+        style={{ backgroundImage: `url(${patternBoxSvg})` }}
+      >
+        <div className='container  max-w-full mx-auto sm:px-2'>
+          <div className='flex flex-wrap justify-around '>
+            <div className='w-4/5 md:w-2/5  xl:w-2/5 pr-2 md:pr-4 md:pl-4 mb-4 mt-8 offset-1 xl:mx-1/5 '>
+              <div
+                className='text-3xl lg:text-5xl md:pt-12 pt-4 md:pl-12 pb-12'
+                data-aos='fade-up'
+              >
+                <h2 className='title' data-editor-processed='true'>
+                  We're on a journey.
+                  <br />
+                  Will you join us?
                 </h2>
               </div>
-              <div class='md:w-1/3 text-right  text-white  '>TECHNOLOGY</div>
-            </div>
-            <div class='flex flex-wrap md:items-end sm:items-center'>
-              <div
-                className='text-white text-lg leading-6'
-                data-editor-processed='true'
+              <h4
+                className='  md:pl-12 lg:text-3xl text-xl '
                 data-aos='fade-up'
                 data-aos-delay='100'
+                data-editor-processed='true'
               >
-                Powered by SWITCH proprietary tech and the latest innovations in
-                electromobility.
-              </div>
-              <div className=' pb-10 md:text-end text-center mt-6 ' data-aos='fade-up'
-              data-aos-delay='200'>
-                <Link
-                  to='/tech'
-                  className='relative px-8 py-2 text-xl lg:text-2xl overflow-hidden font-medium text-white border-[3px] border-gray-100 shadow-inner group'
-                >
-                  <span className='absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-indigo-400 group-hover:w-full ease'></span>
-                  <span className='absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-indigo-400 group-hover:w-full ease'></span>
-                  <span className='absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-indigo-400 group-hover:h-full ease'></span>
-                  <span className='absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-indigo-400 group-hover:h-full ease'></span>
-                  <span className='absolute inset-0 w-full h-full duration-300 delay-300 bg-white opacity-0 group-hover:opacity-100'></span>
-                  <span className='relative transition-colors duration-300 delay-200 group-hover:text-indigo-500 ease'>
-                    DISCOVER
-                  </span>
-                </Link>
-              </div>
+                Contact us and be the first to hear about our EV buses and vans.
+              </h4>
             </div>
-          </div> */}
+
+            <div
+              data-aos='fade-up'
+              data-aos-delay='300'
+              className='md:w-1/2 md:px-4 px-5 xl:w-1/3 xl:mx-1/6 xl:pl-0'
+            >
+             <SubmitForm/>
+            </div>
+          </div>
         </div>
       </section>
     </div>
