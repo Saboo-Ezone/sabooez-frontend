6bbd72824dfcf60f9acb7e1d425734f98ff67039
salman170 <salmanansari02120@gmail.com>
2023-05-25 09:24:46 +0530

    metrocity done

--- src/components/vehicles/Metrocity.jsx
+++ src/components/vehicles/Metrocity.jsx
@@ -4,6 +4,7 @@ import logoMetrocity from '../../assets/vehicle/metrocity/metrocity-logo-white.p
 // import video from "../../assets/video/FIRST_YORK _B-ROLL.mp4"
 import homeMobileBanner from '../../assets/vehicle/metrocity/metrocity-banner-mobile1.jpg';
 import metrocityFirstBritish from '../../assets/vehicle/metrocity/metrocity-first-british.jpg';
+import metrocityEfficient from '../../assets/vehicle/metrocity/metrocity-efficient-class.jpg';
 
 const Metrocity = () => {
   return (
@@ -32,19 +33,23 @@ const Metrocity = () => {
           className='absolute bottom-10 left-4 h-10 brightness-200 contrast-200 md:h-12 lg:left-6 lg:h-14 xl:left-10 xl:h-16 '
         />
       </section>
-      <section className='mt-2 flex flex-col-reverse md:flex-row bg-[#16b895] text-white'>
+      <section className='mt-2 flex flex-col-reverse bg-[#16b895] text-white lg:flex-row'>
         {/* <span>The bus you've been waiting for.</span> */}
-        <div className='flex w-full flex-col   justify-center  md:w-1/2 md:p-14 lg:p-20 xl:p-28 2xl:h-[70vh] '>
-          <div className='mb-6 p-4 text-4xl lg:font-medium '>
+        <div
+          className=' hidden w-full bg-cover bg-center pb-2 lg:block lg:w-2/3'
+          style={{ backgroundImage: `url(${metrocityFirstBritish})` }}
+        ></div>
+        <div className='flex w-full flex-col   justify-center p-4 lg:h-[75vh]  lg:w-1/3 2xl:h-[80vh]  '>
+          <div className='mb-6 p-4 text-4xl  '>
             The first British-built EV in London
           </div>
           <img
             src={metrocityFirstBritish}
             alt='Metrodecker Lightest'
             srcset=''
-            className='p-4 md:hidden '
+            className='p-4 lg:hidden '
           />
-          <div className='px-4 text-justify font-roboto 2xl:text-lg'>
+          <div className='px-4 pb-4 lg:pb-0 text-justify font-roboto 2xl:text-lg'>
             The SWITCH metrocity was the first British-built, pure electric bus
             to enter service on London’s roads in 2014. Since then, it has been
             helping cities around the world move with ease and meet their net
@@ -56,6 +61,35 @@ const Metrocity = () => {
             Its versatile design, unbeatable efficiency and 100% electric
             drivetrain have made it a firm favourite on routes across the UK,
             Europe, and Japan.
+          </div>
+        </div>
+      </section>
+      <section className='mt-2 flex flex-col-reverse md:flex-row  '>
+        {/* <span>The bus you've been waiting for.</span> */}
+        <div className='flex w-full flex-col   justify-center  md:w-1/2 md:p-14 lg:p-20 xl:py-28 xl:pl-28 2xl:h-[70vh] '>
+          <div className='mb-6 p-4 text-4xl font-medium lg:text-5xl '>
+            More miles, less impact
+          </div>
+          <img
+            src={metrocityEfficient}
+            alt='Metrodecker Lightest'
+            srcset=''
+            className='p-4 md:hidden '
+          />
+          <div className='px-4 text-justify font-roboto 2xl:text-lg'>
+            From the city to the suburbs, the SWITCH metrocity is the ideal
+            single-deck bus for all urban environments.
+            <br />
+            <br />
+            At just 7.8-8.8 tonnes and fitted with the latest NMC battery
+            technology, the SWITCH metrocity is the most efficient bus in its
+            class. It can travel up to 190 miles on a single charge, with a full
+            charge complete in just three hours.
+            <br />
+            <br />
+            Whether it’s a crowded city street or an open road, the SWITCH
+            metrocity can help you get where you need to go in the most
+            efficient way possible - without harming the environment.
             <br />
             <br />
           </div>
@@ -65,7 +99,7 @@ const Metrocity = () => {
         </div>
         <div
           className=' hidden w-full bg-cover bg-center pb-2 md:block md:w-1/2'
-          style={{ backgroundImage: `url(${metrocityFirstBritish})` }}
+          style={{ backgroundImage: `url(${metrocityEfficient})` }}
         ></div>
       </section>
     </>
